77ded6e5ea2c638ca90a9ba1498b438698bf3a36
June Leoman Lapera <leomanlapera@gmail.com>
2025-05-06 16:07:35 +0800

    intro fixes

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">Hello!</h1>
-      <p className="mb-4">{`I'm Leoman — a frontend engineer who builds fast, accessible, and scalable web applications and websites. With 8+ years of experience, I craft high-performance digital experiences using modern tools like Next.js, Tailwind CSS, and Sanity. I'm a VS Code user—always focused on clean, maintainable code and a seamless developer experience.`}</p>
+      <p className="mb-4">{`I'm Leoman — a frontend engineer who builds fast, accessible, and scalable web applications and websites. With 12+ years of experience, I craft high-performance digital experiences using modern tools like Next.js, Tailwind CSS, and Sanity. I'm a VS Code user—always focused on clean, maintainable code and a seamless developer experience.`}</p>
       <div className="my-8">
         <BlogPosts />
       </div>
