9adf9d329995b9fddf1a2be1dae28673ce97e943
June Leoman Lapera <leomanlapera@gmail.com>
2025-05-07 11:02:29 +0800

    added domain

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,6 @@
 import { getBlogPosts } from "app/blog/utils";
 
-export const baseUrl = "https://leomandev.vercel.app";
+export const baseUrl = "https://www.leoman.dev";
 
 export default async function sitemap() {
   let blogs = getBlogPosts().map((post) => ({
